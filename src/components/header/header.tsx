5373993a90ecdd2bcb5d93ca04b6a08502cfd7db
BellSharions <bellsharions@gmail.com>
2021-11-19 19:37:25 +0300

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,44 +2,41 @@ import { Link } from "react-router-dom";
 import "./header.scss";
 import { HOME, ABOUT, SIGNIN, SIGNUP, PRODUCTS } from "../../constants/constants";
 
-const Header: React.FC = () => {
-  const test = () => true;
-  return (
-    <header className="header">
-      <h3 className="header__title">Game Market</h3>
-      <div className="navlinks">
-        <Link to={HOME} className="navlinks__link">
-          Home
-        </Link>
-        <div className="dropdown">
-          <div className="button-container">
-            <input className="dropbtn" type="button" value="Products" />
-            <i className="fa fa-caret-down" />
-          </div>
-          <div className="dropdown-content">
-            <Link to={PRODUCTS} className="content__link">
-              PC
-            </Link>
-            <Link to={PRODUCTS} className="content__link">
-              XBox
-            </Link>
-            <Link to={PRODUCTS} className="content__link">
-              PlayStation
-            </Link>
-          </div>
+const Header: React.FC = () => (
+  <header className="header">
+    <h3 className="header__title">Game Market</h3>
+    <div className="navlinks">
+      <Link to={HOME} className="navlinks__link">
+        Home
+      </Link>
+      <div className="dropdown">
+        <div className="button-container">
+          <input className="dropbtn" type="button" value="Products" />
+          <i className="fa fa-caret-down" />
         </div>
-        <Link to={ABOUT} className="navlinks__link">
-          About
-        </Link>
-        <Link to={SIGNIN} className="navlinks__link">
-          Sign-in
-        </Link>
-        <Link to={SIGNUP} className="navlinks__link">
-          Sign-up
-        </Link>
+        <div className="dropdown-content">
+          <Link to={PRODUCTS} className="content__link">
+            PC
+          </Link>
+          <Link to={PRODUCTS} className="content__link">
+            XBox
+          </Link>
+          <Link to={PRODUCTS} className="content__link">
+            PlayStation
+          </Link>
+        </div>
       </div>
-    </header>
-  );
-};
+      <Link to={ABOUT} className="navlinks__link">
+        About
+      </Link>
+      <Link to={SIGNIN} className="navlinks__link">
+        Sign-in
+      </Link>
+      <Link to={SIGNUP} className="navlinks__link">
+        Sign-up
+      </Link>
+    </div>
+  </header>
+);
 
 export default Header;
