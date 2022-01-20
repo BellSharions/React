51337cf47346473d69ea75fddad337665561e784
BellSharions <bellsharions@gmail.com>
2022-01-20 14:03:58 +0300

    Small code cleanup

--- src/components/searchBar/searchBarContainer.tsx
+++ src/components/searchBar/searchBarContainer.tsx
@@ -14,58 +14,14 @@ const SearchBarContainer: FC<ProductParams> = ({ platform, age, sort, sortDir, g
   const [isLoading, setLoading] = loaderHook(false);
   const [search] = useSelector((state: ReducerState) => [state.term]);
   const dispatch = useDispatch();
-  // eslint-disable-next-line require-await
-  const updateQuery = async (e: ChangeEvent<HTMLInputElement>) => {
-    setLoading(true);
-    setTimeout(async () => {
-      if (platform === "" || platform === undefined)
-        setList(
-          await (
-            await fetch(
-              `${`${fetchGameQueryLink}` + "?"}${new URLSearchParams({
-                platform: "all games",
-                text: e.target.value,
-                age,
-                sort,
-                sortDir,
-                genre,
-              })}`
-            )
-          ).json()
-        );
-      else if (e.target.value === "")
-        setList(
-          await (
-            await fetch(
-              `${`${fetchGameQueryLink}` + "?"}${new URLSearchParams({ platform, age, sort, sortDir, genre })}`
-            )
-          ).json()
-        );
-      else
-        setList(
-          await (
-            await fetch(
-              `${`${fetchGameQueryLink}` + "?"}${new URLSearchParams({
-                text: e.target.value,
-                platform,
-                age,
-                sort,
-                sortDir,
-                genre,
-              })}`
-            )
-          ).json()
-        );
-      setLoading(false);
-    }, 1000);
-  };
+
   const changeSearch = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch(changeSearchAction(e.target.value));
   };
 
   const debouncedOnChange = debounce(changeSearch, 300);
   useEffect(() => {
-    (async () => {
+    (() => {
       setLoading(true);
       setTimeout(async () => {
         if (platform === "" || platform === ":platform" || platform === "home" || platform === undefined)
